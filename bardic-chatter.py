c8e3d198793f767394f274135680303500adcec6
justin-ichiro-choi <justinch@uw.edu>
2025-12-06 00:55:02 -0800

    Added requirements.txt file, poems folder for storing text files and file iteself.

---
+++ bardic-chatter.py
@@ -0,0 +1,28 @@
+import time
+import pyautogui
+
+import os
+
+dir_fd = os.open('./poems', os.O_RDONLY)
+
+print("bardic-chatter: The only way to recite good tales")
+print("")
+print("To use this, select the poem name, and use the ']' key to progress through each line.")
+print("Make sure you're tabbed into Minecraft, LOTC server!")
+
+# To do
+# Set up exit to type (exit)
+# Set up function to pull data from poems/"talename".txt
+
+
+
+
+
+talename = input("What story would you like to play?")
+
+with open(talename + '.txt', 'r', opener=opener) as f:
+
+    print('This will be written to somedir/spamspam.txt', file=f)
+
+
+os.close(dir_fd)  # don't leak a file descriptor
