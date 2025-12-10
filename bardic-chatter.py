423c66ad72e61061057f318ada3bef3e1d37a0d9
justin-ichiro-choi <justinch@uw.edu>
2025-12-09 23:41:41 -0800

    Trying to adjust loop to handle cancelling a song immedietly. Added seashanty.txt

--- bardic-chatter.py
+++ bardic-chatter.py
@@ -7,22 +7,42 @@ proceed_key = ']'  # proceed to o
 exit_key = 'ctrl+space'  # escape to stop program
 story_loop = True
 
+
+def stop_story():
+    global story_loop
+    story_loop = False
+    keyboard.press_and_release(proceed_key)
+    print("Story will terminate after this line \n")
+
+
+keyboard.add_hotkey(exit_key, stop_story)
+
 while story_loop:
     talename = input("What story would you like to play?")
 
     print(f"To proceed, press {proceed_key}")
     print(f"To exit, press {exit_key}")
 
+    if not story_loop:
+        break
+
     with open(f"poems/{talename}.txt", "r") as file:
         for line in file:
+            if not story_loop:
+                break
             keyboard.wait(proceed_key)
 
-            if keyboard.is_pressed(exit_key):
-                story_loop = False
+            if not story_loop:
                 break
+
             # Press t
             keyboard.press_and_release('t')
 
             keyboard.write(line)
 
+            if story_loop is False:
+                break
+
             time.sleep(0.6)
+keyboard.unhook_all_hotkeys()
+print("\nProgram terminated.")
